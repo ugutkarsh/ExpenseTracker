b29ad0916e3f93a9f7350b4e18f7b93a20f09e2b
ugutkarsh <utkarshgawande01@gmail.com>
2023-03-04 17:54:09 +0530

    some chnages

--- src/ManageExpense/ExpenseForm.js
+++ src/ManageExpense/ExpenseForm.js
@@ -1,25 +1,43 @@
 import { View, Text, StyleSheet } from 'react-native';
 import Input from './Input';
+import { useState } from 'react';
 
 const ExpenseForm = () => {
-    const amountChangeHandler = () => { };
+    const [inputValues, setInputValues] = useState({
+        amount: '',
+        date: '',
+        description: ''
+    });
+
+    const inputChangedHandler = (inputIdentifier, enteredValue) => {
+        setInputValues((curInputValues) => {
+            return {
+                ...curInputValues,
+                [inputIdentifier]: enteredValue
+            };
+        });
+    };
     return (
         <View >
             <Text style={styles.text}>Your Expenses</Text>
             <View style={{ flexDirection: "row", justifyContent: "space-between" }}>
                 <Input label="Amount" textInputConfig={{
                     keyboardType: 'decimal-pad',
-                    onChangeText: amountChangeHandler,
+                    onChangeText: inputChangedHandler.bind(this, 'amount'),
+                    value: inputValues.amount,
                 }} />
                 <Input label="Date" textInputConfig={{
                     placeholder: 'YYYY-MM-DD',
                     maxLength: 10,
-                    onChangeText: () => { }
+                    onChangeText: inputChangedHandler.bind(this, 'date'),
+                    value: inputValues.date,
                 }} />
             </View>
             <View style={{ flexDirection: 'row', marginBottom: 10 }}>
                 <Input label="Description" textInputConfig={{
                     multiline: true,
+                    onChangeText: inputChangedHandler.bind(this, 'description'),
+                    value: inputValues.description,
                 }} />
             </View>
         </View>
