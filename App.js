c9546d0d38a737bb418f0d6343902bc3c8639d1a
ugutkarsh <utkarshgawande01@gmail.com>
2023-03-21 13:44:15 +0530

    changes

--- App.js
+++ App.js
@@ -1,26 +1,25 @@
-import { StatusBar } from 'react-native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { StatusBar } from 'react-native';
 import { hideNavigationBar } from 'react-native-navigation-bar-color';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
-import Entypo from 'react-native-vector-icons/Entypo';
 
 import IconButton from './src/components/UI/IconButton';
-import ExpensesContextProvider from './src/store/expenses-context';
 import { GlobalStyles } from './src/constants/styles';
+import ExpensesContextProvider from './src/store/expenses-context';
 
+import Account from './src/screens/Account';
 import AllExpenses from './src/screens/AllExpenses';
+import Contact from './src/screens/ContactUs';
+import Editprofile from './src/screens/Editprofile';
+import Login from './src/screens/Login';
 import ManageExpenses from './src/screens/ManageExpenses';
 import RecentExpenses from './src/screens/RecentExpenses';
-import SplashScreen from './src/screens/SplashScreen';
-import Login from './src/screens/Login';
-import SignUp from './src/screens/SignUp';
-import Account from './src/screens/Account';
-import Statistics from './src/screens/Statistics';
 import Report from './src/screens/Report';
-import Editprofile from './src/screens/Editprofile';
-import Contact from './src/screens/ContactUs';
+import SignUp from './src/screens/SignUp';
+import SplashScreen from './src/screens/SplashScreen';
+import Statistics from './src/screens/Statistics';
 
 
 const Stack = createNativeStackNavigator();
